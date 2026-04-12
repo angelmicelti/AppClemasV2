7ac421688fb40fb13fefade5d8dffb1fd8659d4e
angelmicelti <angelmicelti@users.noreply.github.com>
2026-04-12 13:28:46 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
    Cache-first strategy with network fallback for offline support
    ============================================================ */
 
-var CACHE_NAME = 'clemas-v2-cache-v1.9';
+var CACHE_NAME = 'clemas-v2-cache-v1.11';
 
 /* Files to cache for offline use */
 var ASSETS_TO_CACHE = [
